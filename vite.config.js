4285b5ee37503f448a195b1c93b99c0637f2e0c0
Baasir-stack <baasir121121@gmail.com>
2024-07-02 15:33:29 +0500

    changed backend url

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://x-clone-backend-gamma.vercel.app",
         changeOrigin: true,
       },
     },
