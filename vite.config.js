4919b1aac5d11a52db31bf14ae9f2409369fa357
Baasir-stack <baasir121121@gmail.com>
2024-07-02 15:55:48 +0500

    a

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   },
+//   build: {
+//     outDir: "dist",
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,8 +19,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  },
-  build: {
-    outDir: "dist",
   },
 });
