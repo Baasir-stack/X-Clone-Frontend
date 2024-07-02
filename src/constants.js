e95bb54f0834c8147a104010cfc22d6f7d3280ac
Baasir-stack <baasir121121@gmail.com>
2024-07-02 15:55:02 +0500

    change strategy of base url

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://x-clone-backend-gamma.vercel.app";
--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://x-clone-backend-gamma.vercel.app",
-        changeOrigin: true,
-      },
-    },
   },
   build: {
     outDir: "dist",
